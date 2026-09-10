0e1405c6a4d06afc347072d8fcd39f697ef523eb
Vladyslav-Moshkevych <moskevic.v.t@gmail.com>
2026-09-10 20:10:19 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,37 @@
+print("Hello, World!")
+
+# Створення змінних
+a = 10
+b = 5.5
+c = "Python"
+d = True
+e = 3 + 2j
+
+# Виведення значень змінних та їх типів
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+
+# Арифметичні оператори
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** 2)
+
+# Оператори порівняння
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+# Логічні оператори
+print(a > b and b > 0)
+print(a > b or b < 0)
+print(not(a > b))
